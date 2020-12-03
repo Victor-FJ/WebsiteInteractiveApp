2f0abb7894a5aba936188778f844f52346752e1c
Tristan Taylor Kamp <tris0032@edu.easj.dk>
2020-12-03 11:35:24 +0100

    fuck this shit

--- WebsiteInteractiveApp/Selenium.cs
+++ WebsiteInteractiveApp/Selenium.cs
@@ -1,15 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
 using System.Text;
+using System.Threading;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Support.UI;
+
 
 namespace WebsiteInteractiveApp
 {
     public class Selenium : IDisposable
     {
-        private static readonly string DriverDirectory = @"C:\Users\victo\Documents\Zeeland\Div\Selenium";
+        private static readonly string DriverDirectory = @"C:\Users\trist\Documents\skole\selenium";
         private static IWebDriver _driver;
 
         public Selenium()
@@ -37,23 +41,70 @@ namespace WebsiteInteractiveApp
         public bool IsClassInSchool()
         {
             FileManager manager = new FileManager("LogInFile.txt");
-            const string url = "https://ums.easj.dk/WebTimeTable/default.aspx";
+            const string url = "https://cloud.timeedit.net/zealand/web/";
 
             if (_driver.Title != "Skema Web")
             {
                 _driver.Navigate().GoToUrl(url);
 
-                if (_driver.Title != "Log på")
-                    throw new NotSupportedException("Unknown Website");
+                var items = _driver.FindElements(By.ClassName("items"));
+
+                IWebElement enterButton = items[2];
+                enterButton.Click();
+
                 IWebElement userNameElement = _driver.FindElement(By.Id("userNameInput"));
                 userNameElement.Clear();
                 userNameElement.SendKeys(manager.UserName);
+
+                IWebElement nextButton = _driver.FindElement(By.Id("nextButton"));
+                nextButton.Click();
+
+
                 IWebElement passwordElement = _driver.FindElement(By.Id("passwordInput"));
                 passwordElement.Clear();
                 passwordElement.SendKeys(manager.Password);
 
                 IWebElement submitButton = _driver.FindElement(By.Id("submitButton"));
                 submitButton.Click();
+
+                _driver.Navigate().GoToUrl(url+"3/ri1Q7.html");
+
+                IWebElement searchName = _driver.FindElement(By.Id("ffsearchname"));
+                searchName.SendKeys("re");
+
+                Thread.Sleep(500);
+
+                bool x = true;
+                int index = 0;
+                while (x = true)
+                {
+
+                    for (int i = 0; i < 20; i++)
+                    {
+                        try
+                        {
+                            IWebElement viewButton = _driver.FindElement(By.ClassName("loadNextPage"));
+                            viewButton.Click();
+                        }
+                        catch (Exception e)
+                        {
+                            break;
+                        }
+
+                    }
+
+                    void JavaScriptClick(IWebElement element)
+                    {
+                        IJavaScriptExecutor executor = (IJavaScriptExecutor)_driver;
+                        executor.ExecuteScript("document.querySelector(\"#info3 > div.infoboxtitle\").click();", element);
+                    }
+
+
+                }
+
+
+
+
             }
 
             IWebElement listElement = _driver.FindElement(By.Id("day0Col"));
